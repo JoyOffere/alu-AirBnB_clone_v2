804e302983905104dae85529663a2a8fab48f02f
JoyOffere <j.offere@alustudent.com>
2024-02-16 14:55:33 +0000

    Linked servers

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,19 +1,40 @@
 #!/usr/bin/python3
-"""1. Compress before sending"""
-
+"""
+Write a Fabric script that generates
+a .tgz archive from the contents of the
+web_static folder of your AirBnB Clone repo,
+using the function do_pack.
+"""
+import fabric.api as fab
 from datetime import datetime
-from fabric.api import local
-from os.path import isdir
 
 
 def do_pack():
-    """TGZ"""
+    """
+    Prototype: def do_pack():
+    All files in the folder web_static must
+    be added to the final archive
+    All archives must be stored in the folder versions
+    (your function should create this folder if it doesn't exist)
+    The name of the archive created must be
+    web_static_<year><month><day><hour><minute><second>.tgz
+    The function do_pack must return the archive path
+    if the archive has been correctly generated.
+    Otherwise, it should return None
+    """
+    date_obj = datetime.now()
+    date_str = date_obj.isoformat()
+    char_rem = "-:.T"
+    count = 0
     try:
-        date = datetime.now().strftime("%Y%m%d%H%M%S")
-        if isdir("versions") is False:
-            local("mkdir versions")
-        file_name = "versions/web_static_{}.tgz".format(date)
-        local("tar -cvzf {} web_static".format(file_name))
-        return file_name
-    except:
-        return None
+        while char_rem[count] in date_str:
+            date_str = date_str.split(char_rem[count])
+            date_str = "".join(date_str)
+            count += 1
+    except IndexError:
+        pass
+    fab.local("tar -czvf web_static_{0}.tgz web_static".format(date_str))
+    fab.local("mkdir -p ./versions/")
+    fab.local("mv *.tgz versions")
+    path = fab.local("cd ./versions/; readlink -f *.tgz", capture=True)
+    return path
